def1eb9b8a2b607ac8dea3bcc32cf45002cefe70
Glymphie <glymphie@protonmail.com>
2022-04-24 17:11:57 +0200

    Creating Index done

--- src/archiver/archiver.py
+++ src/archiver/archiver.py
@@ -6,6 +6,12 @@ from typing import Dict
 from src.helpers import helpers #type: ignore
 from src.logger import config_logger #type: ignore
 
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, 'da_DK.utf8')
+except:
+    pass
+
 class MailArchiver(config_logger.Logger):
     """Archive email
     """
@@ -23,23 +29,76 @@ class MailArchiver(config_logger.Logger):
 
     def update_index(self) -> None:
         self.logger.info('Updating index')
+        index_location = 'MailArchive/index.html'
 
-        htmlstart = '<head>\n<link rel="stylesheet" href="./static/css/custom.css">\n</head>\n'
-        htmltitle = '<h2>\nEmails found by the UBF8T346G9 Parser\n</h2>\n'
-        htmlend = '</body>\n</html>'
 
-        print(self.mails_info)
+        html_start = '<head>\n<link rel="stylesheet" href="./static/css/custom.css">\n</head>\n'
+        html_title = '<h2>\nEmails found by the UBF8T346G9 Parser\n</h2>\n'
+        html_end = '</body>\n</html>\n'
+        html_year = '<details>\n<summary>{year}</summary>\n{months}\n</details>\n'
+        html_month = '<details>\n<summary>{month}</summary>\n{days}\n</details>\n'
+        html_day = '<details>\n<summary>{day}</summary>\n{topics}\n</details>\n'
+        html_topic_entry = '''<p>\n<a href='{path}'>{subject}</a> - id: {id}\n</p>\n'''
+
+        years_to_write = []
+        for year in sorted(self.mails_info.keys()):
+
+            months_for_the_year = []
+            for month in sorted(self.mails_info[year].keys()):
+
+                days_for_the_month = []
+                for day in sorted(self.mails_info[year][month].keys()):
+
+                    topics_for_the_day = []
+                    for topic in self.mails_info[year][month][day]:
+                        topics_for_the_day.append(html_topic_entry.format(
+                            path=topic.get('path'),
+                            subject=topic.get('subject'),
+                            id=topic.get('id'),
+                        ))
+
+                    days_for_the_month.append(
+                        html_day.format(day=day, topics=''.join(topics_for_the_day)))
+
+                months_for_the_year.append(
+                    html_month.format(month=datetime.strptime(month,'%m').strftime('%B'), days=''.join(days_for_the_month)))
+
+            years_to_write.append(
+                html_year.format(year=year, months=''.join(months_for_the_year)))
+
+        index_content = html_start + html_title + ''.join(years_to_write) + html_end
+
+        with open(index_location, 'w') as f:
+            f.write(index_content)
 
     def _update_mails_info(self, mail, mail_times):
+        year = mail_times.get('year')
+        month = mail_times.get('month')
+        day = mail_times.get('day')
+        mailid = mail.get('id')
+
         mail_info = {
             'path': 'Mails/{year}/{month}/{day}/{mailid}.html'.format(
-                year=mail_times.get('year'),
-                month=mail_times.get('month'),
-                day=mail_times.get('day'),
-                mailid=mail.get('id')
+                year=year,
+                month=month,
+                day=day,
+                mailid=mailid
             ),
-            'subject': mail.get('subject')
+            'subject': mail.get('subject'),
+            'id': mailid
         }
+
+        if not self.mails_info.get(year):
+            self.mails_info[year] = {}
+
+        if not self.mails_info[year].get(month):
+            self.mails_info[year][month] = {}
+
+        if not self.mails_info[year][month].get(day):
+            self.mails_info[year][month][day] = [mail_info]
+        elif mail_info not in self.mails_info[year][month][day]:
+            self.mails_info[year][month][day].append(mail_info)
+
 
     def _style_mails(self, mail: Dict, mail_path: str, message: bytes):
 
@@ -51,26 +110,7 @@ class MailArchiver(config_logger.Logger):
             f.write('\n</div>\n')
 
     def _write_meta_data(self, mail: Dict, mail_file):
-        meta_data = '''
-<div class="bordermeta">
-  <p>
-    Subject: <b>{subject}</b>
-  </p>
-  <p>
-    From: {sender}
-  </p>
-  <p>
-    To: {recipients}
-  </p>
-  <p>
-    CC: {cc}
-  </p>
-  <p>
-    Date: {date}
-  </p>
-</div>
-'''
-
+        meta_data = '''\n<div class="bordermeta">\n<p>\nSubject: <b>{subject}</b>\n</p>\n<p>\nFrom: {sender}\n</p>\n<p>\nTo: {recipients}\n</p>\n<p>\nCC: {cc}\n</p>\n<p>\nDate: {date}\n</p>\n</div>\n'''
         sendertext = self._get_sender(mail)
         recipientstext = self._get_recipients(mail)
         timetext = self._get_time(mail)
